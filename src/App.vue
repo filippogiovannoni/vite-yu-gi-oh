760728f421e1e994c8cedc85c963fb12be3788ea
filippogiovannoni <filippogiovannonii@gmail.com>
2024-02-08 18:13:22 +0100

    Initial commit add header component and API

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+
+import axios from 'axios'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      cards: []
+    }
+  },
+  mounted() {
+    axios.
+      get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then((response => {
+        console.log(response);
+        console.log(response.data);
+        console.log(response.data.data); // Card Info
+
+        this.cards = response.data.data
+
+        console.log(this.cards);
+
+      }))
+  },
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <main></main>
+  <footer></footer>
+</template>
+
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+
+
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <nav class="d-flex">
+            <div class="logo">
+                <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+                    alt="logo">
+            </div>
+            <h1>Yu-Gi-Oh API</h1>
+        </nav>
+    </header>
+</template>
+
+
+<style scoped>
+nav {
+    background-color: var(--yugioh-white);
+    align-items: center;
+}
+
+.logo {
+    width: 80px;
+    padding: 1rem;
+
+    & img {
+        width: 100%;
+    }
+}
+</style>
